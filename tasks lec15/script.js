f4972f78229d3c8f7a68bc1e60f72e6d311f17f8
Mohamed-Elkhawalka <ms4680601@gmail.com>
2026-04-10 18:20:20 +0200

    Add files via upload

---
+++ tasks lec15/script.js
@@ -0,0 +1,11 @@
+var x = Number(prompt("Enter the first number"));
+var y = Number(prompt("Enter the second number"));
+var z = x + y;
+var first_name = prompt("Enter your fist name");
+var last_name = prompt("Enter your last name");
+var full_name = first_name + " " + last_name;
+console.log("sum of the number = " + z);
+console.log("sum of the number = " + (x + y));
+console.log("your name is " + full_name);
+console.log("your name is " + (first_name + " " + last_name));
+
